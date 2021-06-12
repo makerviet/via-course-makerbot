547bd5cb6d1d9049f5696b47f05479fbf52b6c5b
Viet Anh Nguyen <vietanh.dev@gmail.com>
2021-06-12 21:05:05 +0700

    Refactor code format

--- firmware/makerbot_fw/src/main.cpp
+++ firmware/makerbot_fw/src/main.cpp
@@ -9,24 +9,23 @@
 // #define DC_Motor_4
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-WiFiUDP Udp; // Creation of wifi Udp instance
+WiFiUDP Udp;  // Creation of wifi Udp instance
 char packetBuffer[255];
 unsigned int localPort = 9999;
 
-unsigned long lastSignalTime; // Last signal from computer
+unsigned long lastSignalTime;  // Last signal from computer
 
 const char *ssid = "VIA-MakerBot-01";
-const char *password = "makerbotisnotthatfun";
+const char *password = "makerbotisfun";
 IPAddress netMsk(255, 255, 255, 0);
 IPAddress apIP(192, 168, 4, 200);
 IPAddress ipMakerbot(192, 168, 4, 100);
 IPAddress controlIP(192, 168, 4, 200);
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
     WiFi.begin(ssid, password);
-    WiFi.mode(WIFI_STA); // ESP-32 as client
+    WiFi.mode(WIFI_STA);  // ESP-32 as client
     WiFi.config(ipMakerbot, apIP, netMsk);
     Udp.begin(localPort);
 
@@ -36,19 +35,19 @@ void setup()
     pwm.setPWMFreq(1000);
     Wire.setClock(400000);
 }
-// 'state' is the motor directions, the real direction also depends on how mottor wires are
-// connectted to the motorshield
-void setPWM(int chan1, int chan2, bool state, uint16_t val)
-{
-    Serial.print(val);
-    Serial.print("\t");
-    Serial.println(state);
-    if (state) //state = 1 clockwise rotation
+
+
+// 'state' is the motor directions, the real direction also depends on how
+// mottor wires are connected to the motorshield
+void setPWM(int chan1, int chan2, bool state, uint16_t val) {
+    // Serial.print(val);
+    // Serial.print("\t");
+    // Serial.println(state);
+    if (state)  // state = 1 clockwise rotation
     {
         pwm.setPWM(chan1, 0, val);
         pwm.setPWM(chan2, 4096, 0);
-    }
-    else //state = 0 couter-clockwise rotation
+    } else  // state = 0 couter-clockwise rotation
     {
         pwm.setPWM(chan2, 0, val);
         pwm.setPWM(chan1, 4096, 0);
@@ -58,8 +57,7 @@ void setPWM(int chan1, int chan2, bool state, uint16_t val)
 // Value range: [-100, 100]
 // 0 -> 100: Forward
 // 0 -> -100: Backward
-void controlWheels(int16_t leftWheel, int16_t rightWheel)
-{
+void controlWheels(int16_t leftWheel, int16_t rightWheel) {
     leftWheel = leftWheel * 40.96;
     rightWheel = rightWheel * 40.96;
 
@@ -70,40 +68,33 @@ void controlWheels(int16_t leftWheel, int16_t rightWheel)
     setPWM(DC_Motor_RIGHT, 0x8000 & rightWheel, abs(rightWheel));
 }
 
-void loop()
-{
+void loop() {
     // Receive control commands
-    int packetSize = Udp.parsePacket(); // Size of packet to receive
-    if (packetSize)
-    { // If we received a package
+    int packetSize = Udp.parsePacket();  // Size of packet to receive
+    if (packetSize) {                    // If we received a package
 
         // Stop if the control signal is lost
-        if (millis() - lastSignalTime > 1000)
-        {
+        if (millis() - lastSignalTime > 1000) {
             controlWheels(0, 0);
         }
 
         // Parse UDP packet
         int len = Udp.read(packetBuffer, 255);
-        if (len > 0)
-            packetBuffer[len - 1] = 0;
+        if (len > 0) packetBuffer[len - 1] = 0;
         char *token = strtok(packetBuffer, " ");
         if (strcasecmp(token,
-                       "SET_CONTROL_IP"))
-        { // Packet to set controller's IP
+                       "SET_CONTROL_IP")) {  // Packet to set controller's IP
             token = strtok(0, " ");
             controlIP.fromString(String(token));
-        }
-        else if (strcasecmp(token,
-                            "CONTROL_WHEEL"))
-        { // Packet to set wheel speed
+        } else if (strcasecmp(token,
+                              "CONTROL_WHEEL")) {  // Packet to set wheel speed
             token = strtok(0, " ");
             int leftWheel = atoi(token);
             int rightWheel = atoi(token);
             controlWheels(leftWheel, rightWheel);
         }
         lastSignalTime =
-            millis(); // Save the last time receiving signal from controller
+            millis();  // Save the last time receiving signal from controller
     }
 
     // Send some data: IMU, sensors?
