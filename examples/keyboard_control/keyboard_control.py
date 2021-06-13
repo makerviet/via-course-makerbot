ec0ace00dc6458d0856f3431061f29616d19b46a
Viet Anh Nguyen <vietanh.dev@gmail.com>
2021-06-13 21:10:05 +0700

    Improve controller UI

--- examples/keyboard_control/keyboard_control.py
+++ examples/keyboard_control/keyboard_control.py
@@ -2,6 +2,7 @@ import pygame
 import socket
 
 pygame.init()
+pygame.font.init()
 pygame.display.set_caption('VIABot controller')
 window = pygame.display.set_mode((300, 300))
 clock = pygame.time.Clock()
@@ -26,6 +27,7 @@ rect = pygame.Rect(0, 0, 20, 20)
 rect.center = window.get_rect().center
 origin_x = rect.x
 origin_y = rect.y
+
 run = True
 while run:
     clock.tick(20)
@@ -38,22 +40,22 @@ while run:
     keys = pygame.key.get_pressed()
 
     color = (255, 0, 0)
-    if keys[pygame.K_UP]:
+    if keys[pygame.K_UP] or keys[ord('w')]:
         rect.x = origin_x
         rect.y = origin_y - 50
         set_speed(30, 30)
         color = (0, 0, 255)
-    elif keys[pygame.K_DOWN]:
+    elif keys[pygame.K_DOWN] or keys[ord('s')]:
         rect.x = origin_x
         rect.y = origin_y + 50
         set_speed(-30, -30)
         color = (0, 0, 255)
-    elif keys[pygame.K_LEFT]:
+    elif keys[pygame.K_LEFT] or keys[ord('a')]:
         rect.x = origin_x - 50
         rect.y = origin_y
         set_speed(-30, 30)
         color = (0, 0, 255)
-    elif keys[pygame.K_RIGHT]:
+    elif keys[pygame.K_RIGHT] or keys[ord('d')]:
         rect.x = origin_x + 50
         rect.y = origin_y
         set_speed(30, -30)
@@ -67,8 +69,15 @@ while run:
     rect.centerx = rect.centerx % window.get_width()
     rect.centery = rect.centery % window.get_height()
 
-    window.fill(0)
+    window.fill((60, 60, 60))
+
+    pygame.draw.circle(window, (255, 255, 255), (window.get_width() // 2, window.get_height() // 2), 30)
     pygame.draw.rect(window, color, rect)
+    
+    font = pygame.font.SysFont('Comic Sans MS', 16)
+    text = font.render('Use arrow keys or w,a,s,d to move', False, (0, 255, 0))
+    window.blit(text, (10, 10))
+
     pygame.display.flip()
 
 pygame.quit()
